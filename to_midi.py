614cc5918edd76fa985ef19fb666b2e99ba9da03
johahols <johannes.holstad@gmail.com>
2018-11-07 12:34:46 +0100

    +

--- to_midi.py
+++ to_midi.py
@@ -1,7 +1,7 @@
 import mido
 from mido import MidiFile, MidiTrack, Message
 import numpy as np
-
+import pprint
 mid = MidiFile('/home/johannes/github/in5490_AI/sorted_violin_midis/A_major/trio_IV.mid') 
 notes = []
 velocities = []
@@ -9,7 +9,6 @@ velocities = []
 def midi_to_int():
     for msg in mid:
         if not msg.is_meta:
-            print(msg)
             if msg.time == 0:
                 continue
             if msg.channel == 0:
@@ -18,9 +17,6 @@ def midi_to_int():
                     # append note and velocity from [type, note, velocity]
                     notes.append(data[1])
                     velocities.append(data[2])      
-    combine = [[i,j] for i,j in zip(notes, velocities)]
-    int_to_midi(combine)
-
 def pred_to_midi(prediction):
     mid = MidiFile()
     track = MidiTrack()
@@ -29,7 +25,6 @@ def pred_to_midi(prediction):
         # 147 means note_on
         note = np.asarray([147, note[0], note[1]])
         bytes = note.astype(int)
-        print(note)
         msg = Message.from_bytes(bytes[0:3])
         t += 1
         msg.time = t
@@ -43,5 +38,18 @@ midi_to_int()
 new_notes = []
 for i in range(127):   
     new_notes.append(i)
-combine = [[i,j] for i,j in zip(new_notes, velocities[:127])]
-pred_to_midi(combine)
+combine = [[i,j] for i,j in zip(notes, velocities)]
+#pred_to_midi(combine)
+
+
+'''
+note_min = np.min(notes)
+note_max = np.max(notes)
+
+for line in data:
+    for i in line:
+        #minmax scaling (0-1)
+        i = (i- note_min)/(note_max-note_min)
+        #Reverse minmax scaling
+        i = int(i*(note_max - note_min) + note_min)
+'''
